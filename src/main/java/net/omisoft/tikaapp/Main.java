eee307730d43f3ecafe47256f7ad8559246f1722
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-10 00:23:27 +0300

    test files

--- src/main/java/net/omisoft/tikaapp/Main.java
+++ src/main/java/net/omisoft/tikaapp/Main.java
@@ -11,25 +11,21 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
-import static net.omisoft.tikaapp.utils.PropertiesHolder.inputFolderName;
-import static net.omisoft.tikaapp.utils.PropertiesHolder.outputFolderName;
-
 
 public final class Main {
     private static org.apache.log4j.Logger log = Logger.getLogger(Main.class);
     public static String fileName;
+    private static final String inputFolderName = "files";
+
 
 
     public static void main(String... aArgs) {
         PropertiesHolder.init(aArgs);
         PropertyConfigurator.configure("log4j.properties");
-        log.info("Processing folder " + inputFolderName + ", Report file dir: " + outputFolderName);
+        log.info("Starting search...");
         // create the actual filename and delete the existing if exists
-        fileName = outputFolderName + "/" + new SimpleDateFormat("yyyyMMdd").format(new Date());
+        fileName = "report_" + new SimpleDateFormat("yyyyMMdd").format(new Date());
         new File(fileName).delete();
-        new File(outputFolderName).mkdirs();
         ReportWriter reportGenerator = ReportWriter.getInstance();
         reportGenerator.createFileIfNotExists();
         FileProvider fileProvider = new FileProvider();
